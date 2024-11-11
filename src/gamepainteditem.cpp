5a210d064696aebb1708b9b28ae46ff1cf9c3c98
radikru96 <radikru96@gmail.com>
2024-11-12 02:05:18 +0300

    renames & debug.. on develope...

--- src/gamepainteditem.cpp
+++ src/gamepainteditem.cpp
@@ -3,96 +3,129 @@
 #include "game.h"
 #include <QPainter>
 
+uint GamePaintedItem::m_sWidth = 3;
+uint GamePaintedItem::m_sHeight = 3;
+uint GamePaintedItem::m_sWinLength = 3;
+uint GamePaintedItem::m_sFirstPlayer = 1;
+uint GamePaintedItem::m_sPlayers = 2;
+
 GamePaintedItem::GamePaintedItem() {
-    if ( game == nullptr )
-        game = new Game( QSize(mWidth, mHeight), mWinLength, mFirstPlayer, mPlayers );
+    if ( m_pGame == nullptr )
+        m_pGame = new Game( QSize(m_sWidth, m_sHeight), m_sWinLength, m_sFirstPlayer, m_sPlayers );
     this->setMipmap(true);
     this->setAntialiasing(true);
-    nPlayersPic.append(new QImage(":/image/Y.jpg"));
-    nPlayersPic.append(new QImage(":/image/A.jpg"));
+    m_nPlayersPic.append(new QImage(QImage(":/images/y.jpg").scaled(getCellWidth(), getCellHeight() )) );
+    m_nPlayersPic.append(new QImage(QImage(":/images/a.jpg").scaled(getCellWidth(), getCellHeight() )) );
+    m_pGame->move(QSize(0,0),m_sFirstPlayer);
+    m_pGame->move(QSize(2,2),m_sFirstPlayer+1);
 }
 
 GamePaintedItem::~GamePaintedItem()
 {
-    if ( game != nullptr )
-        delete game;
+    if ( m_pGame != nullptr )
+        delete m_pGame;
+    for ( int i = 0; i < m_nPlayersPic.size(); ++i )
+        delete m_nPlayersPic[i];
 }
 
 void GamePaintedItem::paint(QPainter *painter)
 {
+    qreal cellWidth = getCellWidth();
+    qreal cellHeight = getCellHeight();
     painter->save();
-    painter->setBrush(QBrush(QImage(":/images/Z.jpg").scaled(this->width(),this->getHeight())));
-    painter->drawRect(0,0,this->width(),this->getHeight());
-    drawGrid(painter);
+    painter->setBrush(QBrush(QImage(":/images/z.jpg").scaled(this->width(),this->height())));
+    painter->drawRect(0,0,this->width(),this->height());
+    drawCells(painter, cellWidth, cellHeight);
+    drawGrid(painter, cellWidth, cellHeight);
     painter->restore();
 }
 
-void GamePaintedItem::setWidth(uint &newWidth)
-{
-    mWidth = newWidth;
-}
-
-void GamePaintedItem::setHeight(uint &newHeight)
-{
-    mHeight = newHeight;
-}
-
-void GamePaintedItem::setWinLength(uint &newWinLength)
-{
-    mWinLength = newWinLength;
-}
-
-void GamePaintedItem::setFirstPlayer(uint &newFirstPlayer)
-{
-    mFirstPlayer = newFirstPlayer;
-}
-
-void GamePaintedItem::setPlayers(uint &newPlayers)
-{
-    mPlayers = newPlayers;
-}
-
-uint &GamePaintedItem::getWidth() const
-{
-    return mWidth;
-}
-
-uint &GamePaintedItem::getHeight() const
-{
-    return mHeight;
-}
-
-uint &GamePaintedItem::getWinLength() const
-{
-    return mWinLength;
-}
-
-uint &GamePaintedItem::getFirstPlayer() const
-{
-    return mFirstPlayer;
-}
-
-uint &GamePaintedItem::getPlayers() const
-{
-    return mPlayers;
-}
-
-void GamePaintedItem::drawGrid(QPainter *painter)
+void GamePaintedItem::drawGrid(QPainter *painter, const qreal &cellWidth, const qreal &cellHeight)
 {
     painter->save();
     painter->setPen( QPen(Qt::gray,0) );
-    for ( int i = 1 ; i < m_pGame->getWidth(); ++i ) {
-        painter->drawLine(this->width()/m_pGame->getWidth() * i, 0,
-                          this->width()/m_pGame->getWidth() * i, this->getHeight() );
+    for ( uint i = 1 ; i < m_pGame->getWidth(); ++i ) {
+        painter->drawLine(cellWidth * i, 0,
+                          cellWidth * i, this->height() );
     }
-    for ( int i = 1; i < m_pGame->getHeight(); ++i ) {
-        painter->drawLine(0, this->getHeight()/m_pGame->getHeight() * i,
-                          this->width(), this->getHeight()/m_pGame->getHeight() * i );
+    for ( uint i = 1; i < m_pGame->getHeight(); ++i ) {
+        painter->drawLine(0, cellHeight * i,
+                          this->width(), cellHeight * i );
     }
     painter->restore();
 }
 
-void GamePaintedItem::drawCells(QPainter *painter)
+void GamePaintedItem::drawCells(QPainter *painter, const qreal &cellWidth, const qreal &cellHeight)
 {
+    painter->save();
+    for ( uint i = 0 ; i < m_pGame->getWidth(); ++i ) {
+        for ( uint j = 0; j < m_pGame->getHeight(); ++j ) {
+            if ( m_pGame->getField(i, j) > 0 ) {
+                painter->setBrush(QBrush(*(m_nPlayersPic[ m_pGame->getField(i, j)-1 ]) ) );
+                painter->drawRect(cellWidth * i, cellHeight * i,
+                                  cellWidth, cellHeight);
+            }
+        }
+    }
+    painter->restore();
+}
 
+qreal GamePaintedItem::getCellWidth() const
+{
+    return this->width()/m_pGame->getWidth();
+}
+
+qreal GamePaintedItem::getCellHeight() const
+{
+    return this->height()/m_pGame->getHeight();
+}
+
+void GamePaintedItem::setWidth(const uint &newWidth)
+{
+    m_sWidth = newWidth;
+}
+
+void GamePaintedItem::setHeight(const uint &newHeight)
+{
+    m_sHeight = newHeight;
+}
+
+void GamePaintedItem::setWinLength(const uint &newWinLength)
+{
+    m_sWinLength = newWinLength;
+}
+
+void GamePaintedItem::setFirstPlayer(const uint &newFirstPlayer)
+{
+    m_sFirstPlayer = newFirstPlayer;
+}
+
+void GamePaintedItem::setPlayers(const uint &newPlayers)
+{
+    m_sPlayers = newPlayers;
+}
+
+uint &GamePaintedItem::getWidth()
+{
+    return m_sWidth;
+}
+
+uint &GamePaintedItem::getHeight()
+{
+    return m_sHeight;
+}
+
+uint &GamePaintedItem::getWinLength()
+{
+    return m_sWinLength;
+}
+
+uint &GamePaintedItem::getFirstPlayer()
+{
+    return m_sFirstPlayer;
+}
+
+uint &GamePaintedItem::getPlayers()
+{
+    return m_sPlayers;
 }
--- src/gamepainteditem.h
+++ src/gamepainteditem.h
@@ -15,30 +15,34 @@ public:
     ~GamePaintedItem();
     void paint(QPainter *painter);
 
-    static void setWidth(uint &newWidth = 3);
-    static void setHeight(uint &newHeight = 3);
-    static void setWinLength(uint &newWinLength = 3);
-    static void setFirstPlayer(uint &newFirstPlayer = 1);
-    static void setPlayers(uint &newPlayers = 2);
+protected:
+    void drawGrid(QPainter *painter, const qreal &cellWidth, const qreal &cellHeight);
+    void drawCells(QPainter *painter, const qreal &cellWidth, const qreal &cellHeight);
+    qreal getCellWidth() const;
+    qreal getCellHeight() const;
+
+public:
+    static void setWidth(const uint &newWidth = 3);
+    static void setHeight(const uint &newHeight = 3);
+    static void setWinLength(const uint &newWinLength = 3);
+    static void setFirstPlayer(const uint &newFirstPlayer = 1);
+    static void setPlayers(const uint &newPlayers = 2);
 
 protected:
-    static uint &getWidth() const;
-    static uint &getHeight() const;
-    static uint &getWinLength() const;
-    static uint &getFirstPlayer() const;
-    static uint &getPlayers() const;
-
-    void drawGrid(QPainter *painter);
-    void drawCells(QPainter *painter);
+    static uint &getWidth();
+    static uint &getHeight();
+    static uint &getWinLength();
+    static uint &getFirstPlayer();
+    static uint &getPlayers();
 
     Game *m_pGame = nullptr;
     QList<QImage*> m_nPlayersPic;
 
-    static uint mWidth = 3;
-    static uint mHeight = 3;
-    static uint mWinLength = 3;
-    static uint mFirstPlayer = 1;
-    static uint mPlayers = 2;
+    static uint m_sWidth;
+    static uint m_sHeight;
+    static uint m_sWinLength;
+    static uint m_sFirstPlayer;
+    static uint m_sPlayers;
 };
 
 #endif // GAMEPAINTEDITEM_H
